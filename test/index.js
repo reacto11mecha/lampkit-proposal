618be694857e525c99dae65bc7e2c76bad37f3a2
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-03-20 01:28:59 +0700

    menambahkan folder test

---
+++ test/index.js
@@ -0,0 +1,5 @@
+const Lampkit = require("..");
+const rest = new Lampkit();
+
+// rest.get.learning.content('Javascript', 'algorithm/conversion/HexToRgb.js').then(console.log)
+// rest.get.learning.content('Javascript', 'learn/DOM/004_Methods/img/sebelum2.jpg').then(console.log)
